9477e3715892641e26abea1a7a7a8256c75d0764
Viktor <plutonny1@gmail.com>
2022-02-26 19:53:01 +0300

    new SVG icon: email

--- beta/frames.js
+++ beta/frames.js
@@ -119,6 +119,10 @@ var SVG = {
     simple   :`<svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px">
                  <path d="M0 0h24v24H0V0z" fill="none"/>
                  <path d="M3 12c0 2.21.91 4.2 2.36 5.64L3 20h6v-6l-2.24 2.24C5.68 15.15 5 13.66 5 12c0-2.61 1.67-4.83 4-5.65V4.26C5.55 5.15 3 8.27 3 12zm8 5h2v-2h-2v2zM21 4h-6v6l2.24-2.24C18.32 8.85 19 10.34 19 12c0 2.61-1.67 4.83-4 5.65v2.09c3.45-.89 6-4.01 6-7.74 0-2.21-.91-4.2-2.36-5.64L21 4zm-10 9h2V7h-2v6z"/>
+               </svg>`,
+    email    :`<svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px">
+                 <path d="M0 0h24v24H0V0z" fill="none"/>
+                 <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/>
                </svg>`
 }
 
